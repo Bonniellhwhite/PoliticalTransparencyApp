ad18242cb5bf41fe015725acc6b82daed3996122
Bonnie White <Bonniellhwhite@gmail.com>
2024-02-09 23:01:22 -0800

    Test Layouts added for rep and bill search pages (Temp layout)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/testdbapp/firebaseHelper.kt
+++ app/src/main/java/com/example/testdbapp/firebaseHelper.kt
@@ -4,6 +4,8 @@ import com.google.firebase.FirebaseApp
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        // "This" is the application context, it is passing in this app's info to firebase
+        // the app's credentials are validated in our firebase server
         FirebaseApp.initializeApp(this)
     }
 }
